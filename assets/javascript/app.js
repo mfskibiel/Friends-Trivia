4349d5402b917dbbd548ac0c924e8146b3491594
Mary Frances <mfskibiel@gmail.com>
2019-10-30 22:00:35 -0400

    finished product

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,7 @@ var questionsArray = [{
     answers: ["Joey and Rachel", "Ross and Phoebe", "Ross and Monica", "Joey and Monica"],
     correctAnswer: "Ross and Monica"
 }, {
-    question: "What is Joey's favorite food that is often mentioned through the show?",
+    question: "What is Joey's favorite food that is often mentioned throughout the show?",
     answers: ["sandwiches", "pizza", "chicken wings", "all of the above, Joey loves all foods."],
     correctAnswer: "all of the above, Joey loves all foods."
 }, {
@@ -43,7 +43,7 @@ var questionsArray = [{
 var game = {
     correct: 0,
     incorrect: 0,
-    counter: 20,
+    counter: 60,
     countdown: function () {
         game.counter--;
         $("#counter").html(game.counter); //need to create this
@@ -54,8 +54,9 @@ var game = {
     },
     start: function () {
         timer = setInterval(game.countdown, 1000);
-        $("#subwrapper").prepend("<h2>Time Remaining: <span id='counter'>120</span> Seconds </h2>")
+        $("#subwrapper").prepend("<h2>Time Remaining: <span id='counter'>60</span> Seconds </h2>")
         $("#start").remove();
+        $("#instructions").remove();
         for (let i = 0; i < questionsArray.length; i++) {
             $("#subwrapper").append("<h2>" + questionsArray[i].question + "</h2>");
             for (let y = 0; y < questionsArray[i].answers.length; y++) {
@@ -65,66 +66,67 @@ var game = {
         $("#subwrapper").append("<br><button id='end'>DONE</button>")
     },
     done: function () {
-        $.each($("input[name='question-0]':checked"), function () {
+        $.each($("input[name='question-0']:checked"), function () {
             if ($(this).val() == questionsArray[0].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
         });
-        $.each($("input[name='question-1]':checked"), function () {
+        $.each($("input[name='question-1']:checked"), function () {
             if ($(this).val() == questionsArray[1].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
         });
-        $.each($("input[name='question-2]':checked"), function () {
+        $.each($("input[name='question-2']:checked"), function () {
             if ($(this).val() == questionsArray[2].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
         });
-        $.each($("input[name='question-3]':checked"), function () {
+        $.each($("input[name='question-3']:checked"), function () {
             if ($(this).val() == questionsArray[3].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
         });
-        $.each($("input[name='question-4]':checked"), function () {
+        $.each($("input[name='question-4']:checked"), function () {
             if ($(this).val() == questionsArray[4].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
         });
-        $.each($("input[name='question-5]':checked"), function () {
+        $.each($("input[name='question-5']:checked"), function () {
             if ($(this).val() == questionsArray[5].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
         });
-        $.each($("input[name='question-6]':checked"), function () {
+        $.each($("input[name='question-6']:checked"), function () {
             if ($(this).val() == questionsArray[6].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
         });
-        $.each($("input[name='question-7]':checked"), function () {
+        $.each($("input[name='question-7']:checked"), function () {
             if ($(this).val() == questionsArray[7].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
         });
-
+        console.log("done")
         this.result();
     },
     result: function () {
+        console.log("result")
         clearInterval(timer);
         $("#subwrapper h2").remove();
 
